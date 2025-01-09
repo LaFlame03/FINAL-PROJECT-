d34fac491a703fa9f6a3413d667bfcb0fc34b76f
LaFlame03 <cheikhnaatc14004@gmail.com>
2025-01-09 10:24:26 +0100

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,7 +14,6 @@ def emotion_detector(text_to_analyze):
             'dominant_emotion': None
         }
     
-    # Define the API URL
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 
     # Headers required by Watson NLP API
@@ -22,18 +21,16 @@ def emotion_detector(text_to_analyze):
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
 
-    # Payload containing the text to analyze
+    # the text to analyze
     payload = {"raw_document": {"text": text_to_analyze}}
 
     try:
-        # Sending the POST request
         response = requests.post(url, headers=headers, json=payload, timeout=30)
 
         if response.status_code == 200:
             # Convert response to JSON
             response_data = response.json()
             
-            # Extracting emotions from the response
             emotions = response_data['emotion']['emotion']
             
             # Extracting the required emotions and their scores
